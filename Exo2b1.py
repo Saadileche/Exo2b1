680f3fa744a2901de95e476216d95988cca7ac02
Saadi <mohammed.saadi@student.eicpn.province.namur.be>
2024-12-04 17:17:48 +0000

    Exo2b1

---
+++ Exo2b1.py
@@ -0,0 +1,11 @@
+#faire une boucle qui affiche tous les éléments de la liste en changeant le signe des nombre négatifs
+#eXO2bPY
+   # "
+
+liste=[5,-7,8,-5,12]
+                       
+for chiffre in liste:
+    if chiffre < 0:
+       print(-chiffre)
+    else:
+       print(chiffre)
